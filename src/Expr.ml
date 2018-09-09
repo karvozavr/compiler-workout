f1a2f7d36956f7fd3f18aa8611d30c32545659f3
karvozavr <karvozavr@gmail.com>
2018-09-09 19:44:06 +0300

    Hw1 done.

--- src/Expr.ml
+++ src/Expr.ml
@@ -33,15 +33,46 @@ let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 let update x v s = fun y -> if x = y then v else s y
 
 (* An example of a non-trivial state: *)                                                   
-let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
+let some_state = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
 
 (* Some testing; comment this definition out when submitting the solution. *)
-let _ =
+(* let _ =
   List.iter
     (fun x ->
        try  Printf.printf "%s=%d\n" x @@ s x
        with Failure s -> Printf.printf "%s\n" s
     ) ["x"; "a"; "y"; "z"; "t"; "b"]
+*)
+
+(* Casts boolean to int by following rule:
+   false -> 0
+   true  -> 1
+*)
+let boolToInt b = if b then 1 else 0
+
+(* Casts int to boolean by following rule:
+   0         -> false
+   otherwise -> true
+*)
+let intToBool i = if i != 0 then true else false
+
+(* Evaluate binary operation on arguments.
+   val evalBinop : string -> int -> int
+*)
+let evalBinop op l r = match op with
+    | "+"  -> l + r
+    | "-"  -> l - r
+    | "*"  -> l * r 
+    | "/"  -> l / r
+    | "%"  -> l mod r
+    | "<"  -> boolToInt (l < r)
+    | "<=" -> boolToInt (l <= r)
+    | ">"  -> boolToInt (l > r)
+    | ">=" -> boolToInt (l >= r)
+    | "==" -> boolToInt (l == r)
+    | "!=" -> boolToInt (l != r)
+    | "&&" -> boolToInt (intToBool l && intToBool r)
+    | "!!" -> boolToInt (intToBool l || intToBool r)
 
 (* Expression evaluator
 
@@ -50,5 +81,8 @@ let _ =
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let rec eval s e = match e with 
+    | Const c          -> c 
+    | Var v            -> s v
+    | Binop (op, l, r) -> evalBinop op (eval s l) (eval s r)   
+
