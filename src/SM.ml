cb2d39193cd350fb66972970424329f7044689e6
karvozavr <karvozavr@gmail.com>
2018-10-08 14:19:48 +0300

    Ready for action

--- src/SM.ml
+++ src/SM.ml
@@ -28,7 +28,17 @@ type config = int list * Stmt.config
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
 *)                         
-let rec eval env conf prog = failwith "Not yet implemented"
+let rec eval env (stack, (state, inp, out)) prg = match prg with
+    | []               -> (stack, (state, inp, out))
+    | instruction::prg' -> match instruction with
+        | BINOP op -> eval env (let y::x::stack' = stack in ((Expr.evalBinop op x y)::stack', (state, inp, out))) prg'
+        | CONST v  -> eval env (v::stack, (state, inp, out)) prg'
+        | READ     -> eval env (let z::inp' = inp in (z::stack, (state, inp', out))) prg'
+        | WRITE    -> eval env (let z::stack' = stack in (stack', (state, inp, out @ [z]))) prg'
+        | LD symb  -> eval env ((state symb)::stack, (state, inp, out)) prg'
+        | ST symb  -> eval env (let z::stack' = stack in (stack', (Expr.update symb z state, inp, out))) prg'
+        | _        -> failwith "SM Interpreter: Runtime error."
+
 
 (* Top-level evaluation
 
@@ -53,4 +63,14 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile p = failwith "Not yet implemented"
+let rec compile =
+  let rec expr = function
+  | Expr.Var   x          -> [LD x]
+  | Expr.Const n          -> [CONST n]
+  | Expr.Binop (op, x, y) -> expr x @ expr y @ [BINOP op]
+  in
+  function
+  | Stmt.Seq (s1, s2)  -> compile s1 @ compile s2
+  | Stmt.Read x        -> [READ; ST x]
+  | Stmt.Write e       -> expr e @ [WRITE]
+  | Stmt.Assign (x, e) -> expr e @ [ST x]
